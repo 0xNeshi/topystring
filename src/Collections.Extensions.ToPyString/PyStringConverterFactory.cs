a0a3951d1bce9b4f2a717f23f52c4148f508ec0f
shonmisic <19427053+shonmisic@users.noreply.github.com>
2020-06-17 11:38:40 +0200

    Fix issue with IDictionary not printing correctly

--- src/Collections.Extensions.ToPyString/PyStringConverterFactory.cs
+++ src/Collections.Extensions.ToPyString/PyStringConverterFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Collections.Extensions.ToPyString
@@ -7,17 +8,28 @@ namespace Collections.Extensions.ToPyString
     {
         internal static IStringConverter Create(object source, IEnumerable<object> sourceContainers = default, string prefix = "")
         {
+            if (source != null && source.GetType().IsGenericType)
+            {
+                if (source.GetType().GetGenericTypeDefinition() == typeof(KeyValuePair<,>))
+                {
+                    var key = source.GetType().GetProperty(nameof(KeyValuePair<object,object>.Key)).GetValue(source, null);
+                    var value = source.GetType().GetProperty(nameof(KeyValuePair<object,object>.Value)).GetValue(source, null);
+                    var dictionaryEntry = new DictionaryEntry(key, value);
+                    return new DictionaryEntryPyStringConverter(dictionaryEntry, sourceContainers, prefix);
+                }
+            }
+
             switch (source)
             {
                 case null:
                     return new NullPyStringConverter(prefix);
                 case string str:
                     return new StringPyStringConverter(str, prefix);
+                case DictionaryEntry dictEntry:
+                    return new DictionaryEntryPyStringConverter(dictEntry, sourceContainers, prefix);
                 case KeyValuePair<object, object> keyValuePair:
                     var dictionaryEntry = new DictionaryEntry(keyValuePair.Key, keyValuePair.Value);
                     return new DictionaryEntryPyStringConverter(dictionaryEntry, sourceContainers, prefix);
-                case DictionaryEntry dictEntry:
-                    return new DictionaryEntryPyStringConverter(dictEntry, sourceContainers, prefix);
                 case IDictionary dictionary:
                     return new DictionaryPyStringConverter(dictionary, sourceContainers, prefix);
                 case IEnumerable enumerable:
